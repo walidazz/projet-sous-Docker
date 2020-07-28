d7e07a78dbb8f1a0cf8523c28a3d63450f524586
Walid Azzimani <walidazzimani@gmail.com>
2020-07-28 23:19:37 +0200

    mise en place du back signalement user/comment/article

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -3,8 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Comment;
 use App\Entity\ReportArticle;
+use App\Entity\ReportComment;
+use App\Entity\ReportUser;
+use App\Entity\User;
 use App\Repository\ReportArticleRepository;
+use App\Repository\ReportCommentRepository;
+use App\Repository\ReportUserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -31,16 +37,14 @@ class ReportController extends AbstractController
  public function reportArticle(Article $article, Request $request, EntityManagerInterface $em, ReportArticleRepository $repo)
  {
 
-  $user    = $this->getUser();
-  $motif   = $request->get('_motif');
-  $message = $request->get('_message');
-  // $reportExist = $repo->findBy(['auteur' => $user, 'reportedArticle' => $article]);
-  $reportExist = $article->getReportExist($user);
-  //dd($reportExist);
-
+  $userConnected = $this->getUser();
+  $motif         = $request->get('_motif');
+  $message       = $request->get('_message');
+  $reportExist   = $repo->findBy(['auteur' => $userConnected, 'reportedArticle' => $article]);
+  //$reportExist = $article->getReportExist($user);
   if (!$reportExist) {
    $report = new ReportArticle();
-   $report->setAuteur($user);
+   $report->setAuteur($userConnected);
    $report->setReportedArticle($article);
    $report->setMessage($message);
    $report->setMotif($motif);
@@ -51,15 +55,67 @@ class ReportController extends AbstractController
    $this->addFlash('warning', 'Vous avez déja signalé cet article ! ');
 
   }
-
-  // dd($message);
-
   return $this->redirectToRoute('article_show', ['slug' => $article->getSlug()]);
-
-  // return $this->render(
-  //     //On affiche  une vue twig simple (pas de head ni rien, donc aucun héritage de template...) qui sera intégrée dans la modale.
-  //     'report/reportModal.html.twig',
-  //     array('form' => $form->createView(), 'article' => $article)
-  // );
  }
+
+ /**
+  * @Route("/report/user/{id}/", name="report_user, methods={"POST","GET"})
+  */
+ public function reportUser(User $user, Request $request, EntityManagerInterface $em, ReportUserRepository $repo)
+ {
+
+  $userConnected = $this->getUser();
+  $motif         = $request->get('_motif');
+  $message       = $request->get('_message');
+  $reportExist   = $repo->findBy(['auteur' => $userConnected, 'reported' => $user]);
+  //$reportExist = $article->getReportExist($user);
+  if (!$reportExist) {
+
+   $report = new ReportUser();
+   $report->setAuteur($userConnected);
+   $report->setReported($user);
+   $report->setMessage($message);
+   $report->setMotif($motif);
+   $em->persist($report);
+   $em->flush();
+   $this->addFlash('success', 'Signalement pris en compte ! ');
+  } else {
+   $this->addFlash('warning', 'Vous avez déja signalé cet utilisateur ! ');
+
+  }
+  $referer = $request->headers->get('referer');
+
+  return $this->redirectToRoute($referer);
+ }
+
+ /**
+  * @Route("/report/comment/{id}/", name="report_comment, methods={"POST","GET"})
+  */
+ public function reportComment(Comment $comment, Request $request, EntityManagerInterface $em, ReportCommentRepository $repo)
+ {
+
+  $userConnected = $this->getUser();
+  $motif         = $request->get('_motif');
+  $message       = $request->get('_message');
+  $reportExist   = $repo->findBy(['auteur' => $userConnected, 'reportedComment' => $comment]);
+  //$reportExist = $article->getReportExist($user);
+  if (!$reportExist) {
+
+   $report = new ReportComment();
+   $report->setAuteur($userConnected);
+   $report->setReportedComment($comment);
+   $report->setMessage($message);
+   $report->setMotif($motif);
+   $em->persist($report);
+   $em->flush();
+   $this->addFlash('success', 'Signalement pris en compte ! ');
+  } else {
+   $this->addFlash('warning', 'Vous avez déja signalé ce commentaire ! ');
+
+  }
+  $referer = $request->headers->get('referer');
+
+  return $this->redirectToRoute($referer);
+ }
+
 }
