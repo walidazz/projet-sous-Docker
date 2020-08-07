b2808c0df24b0eec091380d09cfd7d4ae82bfcf5
Walid Azzimani <walidazzimani@gmail.com>
2020-08-07 15:12:57 +0200

    mise en cache de la page d'accueil

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,21 +8,43 @@ use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class ArticleController extends AbstractController
 {
 
+  private $repo;
+  private $cache;
+
+  public function __construct(ArticleRepository $repo, CacheInterface $cache)
+  {
+    $this->repo = $repo;
+    $this->cache = $cache;
+  }
+
+
   /**
    * @Route("/", name="homepage")
    */
-  public function index(ArticleRepository $repo)
+  public function getHomepage()
   {
 
-    $globales = $repo->findBy([], ['createdAt' => 'DESC'], 3);
-    $series   = $repo->findThreeLast('Séries');
-    $films    = $repo->findThreeLast('Films');
-    $animes   = $repo->findThreeLast('Animés');
-    $news = $repo->findThreeLast('Actualités');
+    return  $this->cache->get('homepage', function (ItemInterface $item) {
+      $item->expiresAfter(24 * 3600);
+
+      return $this->renderHomepage();
+    });
+  }
+
+  private function renderHomepage()
+  {
+
+    $globales = $this->repo->findBy([], ['createdAt' => 'DESC'], 3);
+    $series   = $this->repo->findThreeLast('Séries');
+    $films    = $this->repo->findThreeLast('Films');
+    $animes   = $this->repo->findThreeLast('Animés');
+    $news = $this->repo->findThreeLast('Actualités');
 
     return $this->render('article/homepage.html.twig', compact('series', 'films', 'animes', 'globales', 'news'));
   }
@@ -30,10 +52,10 @@ class ArticleController extends AbstractController
   /**
    * @Route("/article/{libelle}", name="article_category_list")
    */
-  public function articlelList($libelle, PaginatorInterface $paginator, Request $request, ArticleRepository $repo)
+  public function articlelList($libelle, PaginatorInterface $paginator, Request $request)
   {
 
-    $query    = $repo->findAllByCategory($libelle);
+    $query    = $this->repo->findAllByCategory($libelle);
     $articles = $paginator->paginate(
       $query,
       $request->query->getInt('page', 1),
@@ -41,14 +63,19 @@ class ArticleController extends AbstractController
     );
     return $this->render('article/list.html.twig', compact('articles', 'libelle'));
   }
+
+
+
+
+
 
   /**
    * @Route("/tags/{libelle}", name="article_tag_list")
    */
-  public function articlelListByTag($libelle, PaginatorInterface $paginator, Request $request, ArticleRepository $repo)
+  public function articlelListByTag($libelle, PaginatorInterface $paginator, Request $request)
   {
     $tag = $libelle;
-    $query    = $repo->findAllByTags($libelle);
+    $query    = $this->repo->findAllByTags($libelle);
     $articles = $paginator->paginate(
       $query,
       $request->query->getInt('page', 1),
@@ -57,67 +84,20 @@ class ArticleController extends AbstractController
 
     return $this->render('article/list.html.twig', compact('articles', 'libelle'));
   }
-
-
 
 
   /**
    * @Route("/article/show/{slug}", name="article_show")
    */
-  public function show(Article $article, ArticleRepository $repo)
+  public function show(Article $article)
   {
-    $sameCategory = $repo->findThreeLast($article->getCategory()->getLibelle());
-    $news         = $repo->findBy([], ['createdAt' => 'DESC'], 3);
+    $sameCategory = $this->repo->findThreeLast($article->getCategory()->getLibelle());
+    $news = $this->repo->findBy([], ['createdAt' => 'DESC'], 3);
+    // $news = $this->cache->get('news', function () {
+    //   return $this->repo->findBy([], ['createdAt' => 'DESC'], 3);
+    // });
+
 
     return $this->render('article/index.html.twig', compact('article', 'sameCategory', 'news'));
   }
-
-  //  /**
-  //   * @Route("/articleAjax/{id}", name="articleAjax" )
-  //   */
-  //  public function articleAjax(Article $article): Response
-  //  {
-  //   return $this->json(['title' => $article->getTitle(), 200]);
-  //  }
-
-  //  /**
-  //   * @Route("/panier/", name="panier_index" )
-  //   */
-  //  public function favorite(SessionInterface $session, ArticleRepository $repo)
-  //  {
-  //   $panier         = $session->get('panier', []);
-  //   $panierWithData = [];
-
-  //   foreach ($panier as $id => $value) {
-
-  //    $panierWithData[] = [
-  //     'product' => $repo->find($id),
-
-  //    ];
-  //   }
-  //   // dd($panierWithData);
-  //   return $this->render(
-  //    'article/favorite.html.twig',
-  //    ['items' => $panierWithData]
-  //   );
-  //  }
-
-  /**
-   * @Route("/panier/add/{id}", name="panier_add" )
-   */
-  //  public function addFavorite($id, SessionInterface $session)
-  //  {
-
-  //   $panier = $session->get('panier', []);
-  //   if (!empty($panier[$id])) {
-  //    $panier[$id]++;
-  //   } else {
-  //    $panier[$id] = 1;
-  //   }
-
-  //   $panier = $session->set('panier', $panier);
-
-  //   return $this->redirectToRoute('panier_index');
-  //  }
-
 }
