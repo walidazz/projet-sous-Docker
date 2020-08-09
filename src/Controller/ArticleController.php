46e5706424fd24d8335d767416ad3ca02bd5cb0c
Walid Azzimani <walidazzimani@gmail.com>
2020-08-09 12:24:47 +0200

    @V1.1

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,96 +9,87 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\Cache\ItemInterface;
 
 class ArticleController extends AbstractController
 {
 
-  private $repo;
-  private $cache;
+    private $repo;
+    private $cache;
 
-  public function __construct(ArticleRepository $repo, CacheInterface $cache)
-  {
-    $this->repo = $repo;
-    $this->cache = $cache;
-  }
+    public function __construct(ArticleRepository $repo, CacheInterface $cache)
+    {
+        $this->repo  = $repo;
+        $this->cache = $cache;
+    }
 
+    // public function getHomepage()
+    // {
 
+    //   return  $this->cache->get('homepage', function (ItemInterface $item) {
+    //     $item->expiresAfter(24 * 3600);
 
-  // public function getHomepage()
-  // {
+    //     return $this->renderHomepage();
+    //   });
+    // }
 
-  //   return  $this->cache->get('homepage', function (ItemInterface $item) {
-  //     $item->expiresAfter(24 * 3600);
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function getHomepage()
+    {
 
-  //     return $this->renderHomepage();
-  //   });
-  // }
+        $globales = $this->repo->findBy([], ['createdAt' => 'DESC'], 3);
 
-  /**
-   * @Route("/", name="homepage")
-   */
-  public function getHomepage()
-  {
+        $series = $this->repo->findThreeLast('Séries');
+        $films  = $this->repo->findThreeLast('Films');
+        $animes = $this->repo->findThreeLast('Animés');
+        $news   = $this->repo->findThreeLast('Actualités');
 
-    $globales = $this->repo->findBy([], ['createdAt' => 'DESC'], 3);
-    $series   = $this->repo->findThreeLast('Séries');
-    $films    = $this->repo->findThreeLast('Films');
-    $animes   = $this->repo->findThreeLast('Animés');
-    $news = $this->repo->findThreeLast('Actualités');
+        return $this->render('article/homepage.html.twig', compact('series', 'films', 'animes', 'globales', 'news'));
+    }
 
-    return $this->render('article/homepage.html.twig', compact('series', 'films', 'animes', 'globales', 'news'));
-  }
+    /**
+     * @Route("/article/{libelle}", name="article_category_list")
+     */
+    public function articlelList($libelle, PaginatorInterface $paginator, Request $request)
+    {
 
-  /**
-   * @Route("/article/{libelle}", name="article_category_list")
-   */
-  public function articlelList($libelle, PaginatorInterface $paginator, Request $request)
-  {
+        $query    = $this->repo->findAllByCategory($libelle);
+        $articles = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            9
+        );
+        return $this->render('article/list.html.twig', compact('articles', 'libelle'));
+    }
 
-    $query    = $this->repo->findAllByCategory($libelle);
-    $articles = $paginator->paginate(
-      $query,
-      $request->query->getInt('page', 1),
-      9
-    );
-    return $this->render('article/list.html.twig', compact('articles', 'libelle'));
-  }
+    /**
+     * @Route("/tags/{libelle}", name="article_tag_list")
+     */
+    public function articlelListByTag($libelle, PaginatorInterface $paginator, Request $request)
+    {
+        $tag      = $libelle;
+        $query    = $this->repo->findAllByTags($libelle);
+        $articles = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            9
+        );
 
+        return $this->render('article/list.html.twig', compact('articles', 'libelle'));
+    }
 
+    /**
+     * @Route("/article/show/{slug}", name="article_show")
+     */
+    public function show(Article $article)
+    {
+        $sameCategory = $this->repo->findThreeLast($article->getCategory()->getLibelle());
+        $news         = $this->repo->findBy([], ['createdAt' => 'DESC'], 3);
+        // $news = $this->cache->get('news', function () {
+        //   return $this->repo->findBy([], ['createdAt' => 'DESC'], 3);
+        // });
 
-
-
-
-  /**
-   * @Route("/tags/{libelle}", name="article_tag_list")
-   */
-  public function articlelListByTag($libelle, PaginatorInterface $paginator, Request $request)
-  {
-    $tag = $libelle;
-    $query    = $this->repo->findAllByTags($libelle);
-    $articles = $paginator->paginate(
-      $query,
-      $request->query->getInt('page', 1),
-      9
-    );
-
-    return $this->render('article/list.html.twig', compact('articles', 'libelle'));
-  }
-
-
-  /**
-   * @Route("/article/show/{slug}", name="article_show")
-   */
-  public function show(Article $article)
-  {
-    $sameCategory = $this->repo->findThreeLast($article->getCategory()->getLibelle());
-    $news = $this->repo->findBy([], ['createdAt' => 'DESC'], 3);
-    // $news = $this->cache->get('news', function () {
-    //   return $this->repo->findBy([], ['createdAt' => 'DESC'], 3);
-    // });
-
-
-    return $this->render('article/index.html.twig', compact('article', 'sameCategory', 'news'));
-  }
+        return $this->render('article/index.html.twig', compact('article', 'sameCategory', 'news'));
+    }
 }
